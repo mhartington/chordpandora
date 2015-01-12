f109ef94e8b7b53684d9eb749e9c7548077d829f
Jason <jasontanner328@gmail.com>
2015-01-12 16:47:43 -0500

    init commit

---
+++ www/js/app.js
@@ -0,0 +1,35 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('chordPandora', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(router);
+
+
+function router($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+  
+  $stateProvider.state('home', {
+  	 url: '/',
+    views: {
+      "header": {templateUrl: 'templates/header.html'},    
+      "body": {templateUrl: 'templates/home.html'}    
+    }
+  })
+
+}
